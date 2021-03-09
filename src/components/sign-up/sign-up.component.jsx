b6ad5848328555c91f1d42e1ccba0b3afbd1fec7
Tomás <tomascatena@gmail.com>
2021-03-09 20:49:15 +0100

    Implement firebase utils, including ability to store authenticated users into our firestore database

---
+++ src/components/sign-up/sign-up.component.jsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import './sign-up.styles.scss';
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
+
+class SignUp extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      displayName: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+    };
+  }
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    const { displayName, email, password, confirmPassword } = this.state;
+    if (password !== confirmPassword) {
+      alert('Passwords do not match');
+      return;
+    }
+
+    try {
+      const { user } = await auth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+
+      await createUserProfileDocument(user, { displayName });
+
+      this.setState({
+        diplayName: '',
+        email: '',
+        password: '',
+        consfirmPassword: '',
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    const { displayName, email, password, confirmPassword } = this.state;
+
+    return (
+      <div className="sign-up">
+        <h2 className="title">I do not have an account</h2>
+        <span>Sign up with your email and password</span>
+        <form className="sign-up-form" onSubmit={this.handleSubmit}>
+          <FormInput
+            type="text"
+            name="displayName"
+            id="displayName"
+            value={displayName}
+            onChange={this.handleChange}
+            label="Display Name"
+            required
+            autoComplete="off"
+          />
+          <FormInput
+            type="email"
+            name="email"
+            id="email"
+            value={email}
+            onChange={this.handleChange}
+            label="Email"
+            required
+            autoComplete="off"
+          />
+          <FormInput
+            type="password"
+            name="password"
+            id="password"
+            value={password}
+            onChange={this.handleChange}
+            label="Password"
+            required
+            autoComplete="off"
+          />
+          <FormInput
+            type="password"
+            name="confirmPassword"
+            id="confirmPassword"
+            value={confirmPassword}
+            onChange={this.handleChange}
+            label="Confirm Password"
+            required
+            autoComplete="off"
+          />
+          <CustomButton type="submit">SIGN UP</CustomButton>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignUp;
